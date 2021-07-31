9cbbf64e1d7680651da628ac9d473d6986e98357
vicllo <vicllo@users.noreply.github.com>
2021-08-01 00:21:47 +0200

    Update Find min recursion.ml
    Bug fix

--- Math/Find min recursion.ml
+++ Math/Find min recursion.ml
@@ -2,5 +2,5 @@
 
 let rec find_min l = match l with
 	|[x] -> x
-	|t::q -> (t if t < (find_min q) else find_min q)
+	|t::q -> if t < (find_min q) then t else find_min q
 	|[] -> failwith "Liste vide!";;
